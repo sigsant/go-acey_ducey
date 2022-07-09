beacd5df8119fdb775ffa06e9b7e6b8ea0def4ab
Heros <eleusis@duck.com>
2022-07-09 22:41:51 +0200

    Delete prints from pkg/card/card.go

--- main.go
+++ main.go
@@ -5,13 +5,17 @@ import (
 	"math/rand"
 	"time"
 
-	"github.com/sigsant/acey_ducey/pkg/card"
+	"github.com/sigsant/acey_ducey/pkg/game"
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	firstCard, secondCard := card.ReturnCard()
-	fmt.Println("Datos de la primera carta", firstCard.ToString())
-	fmt.Println("Datos de la segunda carta", secondCard.ToString())
+	fmt.Println("\n\tAcey-Ducey is played in the following manner: ")
+	fmt.Println("\n\tThe dealer (Computer) deals two cards face up.")
+	fmt.Println("\tYou have an option to bet or not to bet depending on whether or not you feel the card will have a value between the  first two.")
+	fmt.Println(("\tIf you do not want to bet, input an 0"))
+
+	game.StartGame()
+
 }
