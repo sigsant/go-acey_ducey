d84b7acd3cc7ab0cf50ddce9e1af0e8120a2b03a
Heros <eleusis@duck.com>
2022-07-13 16:52:07 +0200

    Add function to add figures to the card

--- pkg/card/card.go
+++ pkg/card/card.go
@@ -3,12 +3,25 @@ package card
 import (
 	"encoding/json"
 	"math/rand"
+	"strconv"
 )
 
 type Card struct {
 	Value int
+	Name  string
 }
 
+type figure int
+
+const (
+	jack = figure(iota + 11)
+	queen
+	king
+	ace
+)
+
+// getCard generate a range of cards from 2 to 14
+// 11 (Jack), 12 (Queen), 13 (King), 14 (Ace)
 func getCard() int {
 	return rand.Intn(14) + 1
 }
@@ -19,6 +32,7 @@ func PlayerCard() *Card {
 	return playerCard
 }
 
+// Dealercards devuelve las dos primeras cartas del dealer
 func DealerCards() (*Card, *Card) {
 	firstCard := &Card{}
 	secondCard := &Card{}
@@ -31,6 +45,8 @@ func DealerCards() (*Card, *Card) {
 			firstCard.Value = getCard()
 			secondCard.Value = getCard()
 		} else {
+			convertFigures(figure(firstCard.Value), firstCard)
+			convertFigures(figure(secondCard.Value), secondCard)
 			break
 		}
 	}
@@ -38,7 +54,24 @@ func DealerCards() (*Card, *Card) {
 	return firstCard, secondCard
 }
 
+// convertFigures añade el nombre de la figura a la carta segun el numero generado
+func convertFigures(f figure, c *Card) {
+	switch f {
+	case jack:
+		c.Name = "Jack"
+	case queen:
+		c.Name = "Queen"
+	case king:
+		c.Name = "King"
+	case ace:
+		c.Name = "Ace"
+	default:
+		c.Name = strconv.Itoa(int(f))
+	}
+}
+
 // ToString convierte el struc en un JSON para su fácil lectura
+// Empleado para testing
 func (c *Card) ToString() string {
 	output, err := json.Marshal(c)
 	if err != nil {
