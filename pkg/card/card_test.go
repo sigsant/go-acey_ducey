8c8399322329bcd6cea3168f8c8e5beabc6437af
Heros <eleusis@duck.com>
2022-07-13 16:52:51 +0200

    Test name of figures of the card

--- pkg/card/card_test.go
+++ pkg/card/card_test.go
@@ -13,16 +13,39 @@ func TestGetCard(t *testing.T) {
 	i := 0
 	for i <= 15 {
 		t.Run(fmt.Sprintf("Lanzada de manos: %d", i), func(t *testing.T) {
-			firstCard, secondCard := ReturnCard()
-
-			if firstCard.Value < secondCard.Value {
-				t.Logf("Primera carta es menor que la segunda. %d vs %d", firstCard.Value, secondCard.Value)
-			} else {
+			firstCard, secondCard := DealerCards()
+			// fmt.Printf("Primera carta: %d \t Segunda carta %d \t", firstCard.Value, secondCard.Value)
+			if firstCard.Value >= secondCard.Value {
 				t.Errorf("Primera carta es igual o mayor a la segunda. %d vs %d", firstCard.Value, secondCard.Value)
 			}
 
 			i++
 		})
+	}
+}
 
+func TestNameCard(t *testing.T) {
+	testCard := Card{}
+	testCases := []struct {
+		value        int
+		expectedName string
+	}{
+		{2, "2"},
+		{11, "Jack"},
+		{5, "5"},
+		{12, "Queen"},
+		{13, "King"},
+		{14, "Ace"},
+	}
+	for _, val := range testCases {
+		t.Run(fmt.Sprintf("The card display is %s", val.expectedName), func(t *testing.T) {
+			convertFigures(figure(val.value), &testCard)
+
+			if testCard.Name == val.expectedName {
+				t.Logf("The card name is %s", val.expectedName)
+			} else {
+				t.Errorf("Value received %s", testCard.Name)
+			}
+		})
 	}
 }
